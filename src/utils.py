d04ebddde53de652c7e83538b2b8e6e913d0b510
HafedhBENAFIA88 <benafia.hafedh1988@gmail.com>
2023-07-28 08:04:39 +0200

    adding grid search CV

--- src/utils.py
+++ src/utils.py
@@ -22,22 +22,22 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def models_evaluation(X_train, y_train,X_test,y_test,models):
+def models_evaluation(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             logging.info(f" looking at the model {list(models.keys())[i]}")
             model = list(models.values())[i]
-            #para = params[list(models.keys())[i]]
 
-            #gs = GridSearchCV(model,para,cv=3)
-            model.fit(X_train,y_train)
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
          
-            #model.set_params(**gs.best_params_)
-            #model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+            model.set_params(**gs.best_params_)
+            
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
